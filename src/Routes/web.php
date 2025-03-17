e140204afe23282bb24b313347683940d430a53d
ValerianMemsk <valera.goncharenko@hotmail.com>
2025-03-17 23:07:07 +0300

    Фикc контроллера

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::controller(42sol\LaravelOnlyoffice\Http\Controllers\OnlyOfficeController::class)
+Route::controller(\sol42\LaravelOnlyoffice\Http\Controllers\OnlyOfficeController::class)
     ->prefix('onlyoffice')
     ->group(function () {
         Route::get('/open', 'open')->name('onlyoffice.open');
